9063aeec7ac938e8b85c42afcc11588b5cf16b0c
Kim McLane <kmmclan09@gmail.com>
2025-07-09 21:09:24 -0400

    feat: added check for name or symbol if number is not provided and output result

--- element.sh
+++ element.sh
@@ -18,4 +18,19 @@ if [[ -z $1 ]]
                     done
             fi
         fi
+
+        if [[ ! $1 =~ ^[0-9]+$ ]]
+      then
+        RESULT=$($PSQL "SELECT * FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE name LIKE '$1' OR symbol LIKE '$1' ORDER BY atomic_number")
+        
+        if [[ -z $RESULT ]]
+          then
+            echo I could not find that element in the database.
+          else
+            echo "$RESULT" | while IFS=\| read TYPE_ID ATOMIC_NUMBER ATOMIC_MASS MELTING_POINT BOILING_POINT SYMBOL NAME TYPE
+              do
+                echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+            done
+          fi
+    fi
 fi
